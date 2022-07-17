bb439bf4b02b4bed0ce73cf49f46d31bfcbfe84f
Jonathan Rigottier <jrigottier@gmail.com>
2022-07-17 11:45:39 +0300

    added models and repositories

---
+++ src/main/java/com/sda/studysystem/repositories/SchoolRepository.java
@@ -0,0 +1,15 @@
+package com.sda.studysystem.repositories;
+
+import com.sda.studysystem.models.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository to handle school related DB operations
+ *
+ * @author Vinod John
+ */
+@Repository
+public interface SchoolRepository extends JpaRepository<School, Long> {
+
+}
