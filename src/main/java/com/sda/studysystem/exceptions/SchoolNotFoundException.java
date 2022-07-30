eeb7bbf6eae360a188fdd63b7354d72855696009
Jonathan Rigottier <jrigottier@gmail.com>
2022-07-30 12:20:45 +0300

    After implementing HTML and create method to school

--- src/main/java/com/sda/studysystem/exceptions/SchoolNotFoundException.java
+++ src/main/java/com/sda/studysystem/exceptions/SchoolNotFoundException.java
@@ -6,7 +6,12 @@ package com.sda.studysystem.exceptions;
  * @author Vinod John
  */
 public class SchoolNotFoundException extends Exception {
+
     public SchoolNotFoundException(Long id) {
         super(String.format("School not found for id: %d", id));
     }
+    public SchoolNotFoundException(String name) {
+        super(String.format("School not found for name: %s", name));
+    }
+
 }
